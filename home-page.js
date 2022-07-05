5be72b5c029b6ad2760713ae8cc80793104d3349
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-06 00:08:03 +0300

    added automatic slideshow to home

--- home-page.js
+++ home-page.js
@@ -1,17 +1,22 @@
 let slideIndex = 0;
-showSlides();
 
-function showSlides(){
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for(i = 0; i < slides.length; i++)
-        slides[i].style.display = "none";
-    slideIndex++;
-    if(slideIndex > slides.length)
-        slideIndex = 1;
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000);
+function showSlidesAuto(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex+=n;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";  
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlidesAuto, 2000); // Change image every 2 seconds
 }
+
 function plusSlides(n){
     showSlides(slideIndex += n);
 }
@@ -34,7 +39,7 @@ function showSlides(n){
         dots[i].className = dots[i].className.replace(" active", "");
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides(n + 1), 4000);
+    showSlidesAuto(slideIndex - 1);
 }
 
 var modal = document.getElementById('id01');
