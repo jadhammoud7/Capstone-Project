0e758b2f902ce19f3f5f9da3aa1809cab5b00b23
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-07-11 14:45:04 +0300

    finished menu but still burger not working

--- home-page.js
+++ home-page.js
@@ -6,3 +6,13 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+const hamBurger=document.querySelector(".hamburger");
+const nav_menu=document.querySelector(".nav-menu");
+hamBurger.addEventListener('click',()=>{
+  hamBurger.classList.toggle('active');
+  nav_menu.classList.toggle('active');
+})
+document.querySelectorAll(".nav-link").forEach(n=> n.addEventListener('click', () =>{
+  hamBurger.classList.remove('active');
+  hamburger.classList.remove('active');
+}))
