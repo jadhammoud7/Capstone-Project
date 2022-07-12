1c97d90f3c024f4f1fa998620fa6e31a000b88d8
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-12 22:48:36 +0300

    fixed the hamburger menu responsive

--- home-page.js
+++ home-page.js
@@ -6,14 +6,14 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-const hamBurger=document.querySelector(".hamburger");
-const nav_menu=document.querySelector(".nav-menu");
+// const hamBurger=document.querySelector(".hamburger");
+// const nav_menu=document.querySelector(".nav-menu");
 
-hamBurger.addEventListener("click",()=>{
-  hamBurger.classList.toggle('active');
-  nav_menu.classList.toggle('active');
-})
-document.querySelectorAll(".nav-link").forEach(n=> n.addEventListener("click", () =>{
-  hamBurger.classList.remove("active");
-  nav_menu.classList.remove("active");
-}))
+// hamBurger.addEventListener("click",()=>{
+//   hamBurger.classList.toggle("active");
+//   nav_menu.classList.toggle("active");
+// })
+// document.querySelectorAll(".nav-link").forEach(n=> n.addEventListener("click", () =>{
+//   hamBurger.classList.remove("active");
+//   nav_menu.classList.remove("active");
+// }))
