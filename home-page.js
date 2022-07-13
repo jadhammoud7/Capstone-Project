09a2b58f058426a939d018030167edb3915515ca
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-14 00:55:33 +0300

    will do animations to pages

--- home-page.js
+++ home-page.js
@@ -21,12 +21,18 @@ function showSlides() {
 const hamBurger = document.querySelector(".hamburger");
 const nav_menu = document.querySelector(".nav-menu");
 const slideshow = document.querySelector(".slideshow-container");
+const feature = document.querySelector(".feature-part");
 
 hamBurger.addEventListener("click", () => {
     hamBurger.classList.toggle("active");
     nav_menu.classList.toggle("active");
 })
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+
     hamBurger.classList.remove("active");
     nav_menu.classList.remove("active");
 }))
+
+
+
+
