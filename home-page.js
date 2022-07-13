b91d4b01c5b6538593bb7452c9534bed27dedb0b
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-13 22:42:52 +0300

    added script to js and fixed it

--- home-page.js
+++ home-page.js
@@ -1,19 +1,32 @@
+let slideIndex = 0;
+showSlides();
 
-var modal = document.getElementById('id01');
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
-// const hamBurger=document.querySelector(".hamburger");
-// const nav_menu=document.querySelector(".nav-menu");
 
-// hamBurger.addEventListener("click",()=>{
-//   hamBurger.classList.toggle("active");
-//   nav_menu.classList.toggle("active");
-// })
-// document.querySelectorAll(".nav-link").forEach(n=> n.addEventListener("click", () =>{
-//   hamBurger.classList.remove("active");
-//   nav_menu.classList.remove("active");
-// }))
+const hamBurger = document.querySelector(".hamburger");
+const nav_menu = document.querySelector(".nav-menu");
+const slideshow = document.querySelector(".slideshow-container");
+
+hamBurger.addEventListener("click", () => {
+    hamBurger.classList.toggle("active");
+    nav_menu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamBurger.classList.remove("active");
+    nav_menu.classList.remove("active");
+}))
