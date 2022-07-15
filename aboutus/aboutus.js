669ece01b3f06ce7458a56b526e9f822596992e9
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-16 02:00:18 +0300

    added animations to about us page

--- aboutus/aboutus.js
+++ aboutus/aboutus.js
@@ -30,3 +30,22 @@ counters.forEach(counter => {
     }
     updateCount();
 })
+
+function revealX(){
+    var reveals = document.querySelectorAll(".reveal-by-x");
+
+    for(var i = 0; i <  reveals.length; i++){
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add("on");
+        }
+        else{
+            reveals[i].classList.remove("on");
+        }
+    }
+}
+
+window.addEventListener("pageshow", revealX);
