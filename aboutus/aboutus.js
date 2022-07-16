4176f811caa212d31315bf24a3c371d1a90052b5
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-16 17:56:03 +0300

    added return to top button to contact and about

--- aboutus/aboutus.js
+++ aboutus/aboutus.js
@@ -68,3 +68,21 @@ function revealY() {
 window.addEventListener("pageshow", revealX);
 window.addEventListener("scroll", revealY);
 window.addEventListener("pageshow", revealY);
+
+var topButton = document.getElementById("TopBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    }
+    else {
+        topButton.style.display = "none";
+    }
+}
+
+function ReturnToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
--- contactus/contactus.js
+++ contactus/contactus.js
@@ -50,3 +50,21 @@ function revealX() {
 window.addEventListener("pageshow", revealX);
 window.addEventListener("pageshow", revealY);
 window.addEventListener("scroll", revealX);
+
+var topButton = document.getElementById("TopBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    }
+    else {
+        topButton.style.display = "none";
+    }
+}
+
+function ReturnToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
