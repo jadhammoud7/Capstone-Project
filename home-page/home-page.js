da70b87d2b9fae4d77848f54907a42be8faf3bd1
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-16 12:08:47 +0300

    completed return to top button js

--- home-page/home-page.js
+++ home-page/home-page.js
@@ -71,6 +71,24 @@ window.addEventListener("scroll", revealX);
 window.addEventListener("scroll", revealY);
 window.addEventListener("pageshow", revealY);
 
+var topButton = document.getElementById("TopBtn");
+
+window.onscroll = function() { scrollFunction()};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        topButton.style.display = "block";
+    }
+    else{
+        topButton.style.display = "none";
+    }
+}
+
+function ReturnToTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 
 
 
