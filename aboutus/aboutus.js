2edc9bf19d50ce0f915341c28b5c94c66c136cab
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-16 16:28:39 +0300

    added paragraphs to about us and animations

--- aboutus/aboutus.js
+++ aboutus/aboutus.js
@@ -48,4 +48,23 @@ function revealX(){
     }
 }
 
+function revealY(){
+    var reveals = document.querySelectorAll(".reveal-by-y");
+
+    for(var i = 0; i <  reveals.length; i++){
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add("on");
+        }
+        else{
+            reveals[i].classList.remove("on");
+        }
+    }
+}
+
 window.addEventListener("pageshow", revealX);
+window.addEventListener("scroll", revealY);
+window.addEventListener("pageshow", revealY);
