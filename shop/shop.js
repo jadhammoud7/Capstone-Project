37bbb4fdb0c83669a4048829b2fa3a4f5b99aa75
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-18 22:56:59 +0300

    added reveal y to shop js css and html

--- shop/shop.js
+++ shop/shop.js
@@ -1,20 +1,35 @@
-let tabs=document.querySelectorAll(".tabs button");
-let tabsArray=Array.from(tabs);
- 
+let tabs = document.querySelectorAll(".tabs button");
+let tabsArray = Array.from(tabs);
 
-let divs=document.querySelectorAll(".content> div ");
-let divArray=Array.from(divs);
 
-tabsArray.forEach((element)=>{
-    element.addEventListener("click",function(e){
-        tabsArray.forEach((element)=>{
-            element.classList.remove("active"); 
+let divs = document.querySelectorAll(".content> div ");
+let divArray = Array.from(divs);
+
+tabsArray.forEach((element) => {
+    element.addEventListener("click", function (e) {
+        tabsArray.forEach((element) => {
+            element.classList.remove("active");
         });
         e.currentTarget.classList.add("active");
-        divArray.forEach((divs)=>{
-            divs.style.display="none";
+        divArray.forEach((divs) => {
+            divs.style.display = "none";
         });
-        document.querySelector(e.currentTarget.dataset.cont).style.display="block"; 
-        });
+        document.querySelector(e.currentTarget.dataset.cont).style.display = "block";
+    });
 });
- 
+
+function revealY() {
+    var reveals = document.querySelectorAll(".reveal-by-y");
+
+    for (var i = 0; i < reveals.length; i++) {
+        if (reveals.style.display = "block") {
+            reveals[i].classList.add("on");
+        }
+        else {
+            reveals[i].classList.remove("on");
+        }
+    }
+}
+
+window.addEventListener("pageshow", revealY);
+window.addEventListener("load", revealY);
