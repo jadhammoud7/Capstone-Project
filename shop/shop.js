c6a1c901cb399e5d73716bae872a83eea37f1983
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-07-18 13:50:54 +0300

    finsished js for tabs

--- shop/shop.js
+++ shop/shop.js
@@ -5,4 +5,16 @@ let tabsArray=Array.from(tabs);
 let divs=document.querySelectorAll(".content> div ");
 let divArray=Array.from(divs);
 
+tabsArray.forEach((element)=>{
+    element.addEventListener("click",function(e){
+        tabsArray.forEach((element)=>{
+            element.classList.remove("active"); 
+        });
+        e.currentTarget.classList.add("active");
+        divArray.forEach((divs)=>{
+            divs.style.display="none";
+        });
+        document.querySelector(e.currentTarget.dataset.cont).style.display="block"; 
+        });
+});
  
