3e770a6031c60d1bc6afbd5d568ef446d54a9c93
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-19 21:39:58 +0300

    applying reveal x 2 animation in home page

--- home-page/home-page.js
+++ home-page/home-page.js
@@ -67,7 +67,25 @@ function revealX() {
     }
 }
 
+function revealX2() {
+    var reveals = document.querySelectorAll(".reveal-by-x2");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("on");
+        }
+        else {
+            reveals[i].classList.remove("on");
+        }
+    }
+}
+
 window.addEventListener("scroll", revealX);
+window.addEventListener("scroll", revealX2);
 window.addEventListener("scroll", revealY);
 window.addEventListener("pageshow", revealY);
 
