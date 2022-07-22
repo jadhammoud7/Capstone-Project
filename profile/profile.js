dbbc2e5c9a598f4960aac5474e845b92671d8a4f
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-23 00:15:19 +0300

    fixed hamburger menu in profile

---
+++ profile/profile.js
@@ -0,0 +1,12 @@
+const hamBurger = document.querySelector(".hamburger");
+const nav_menu = document.querySelector(".nav-menu");
+
+hamBurger.addEventListener("click", () => {
+    hamBurger.classList.toggle("active");
+    nav_menu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+
+    hamBurger.classList.remove("active");
+    nav_menu.classList.remove("active");
+}))
