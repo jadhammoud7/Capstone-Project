5d0e5b45b6cd3590f8017a539fd68666cdd29549
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-24 15:01:34 +0300

    added return to top in profile html js

--- profile/profile.js
+++ profile/profile.js
@@ -89,3 +89,22 @@ closebtn.addEventListener("click", () => {
     console.log("btn closed clicked");
     closeprofile.style.display = "none";
 })
+
+
+var topButton = document.getElementById("TopBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    }
+    else {
+        topButton.style.display = "none";
+    }
+}
+
+function ReturnToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
