5c9ab997555c7d59d122dcb21aec368380712d97
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-25 23:55:26 +0300

    added to js and css basket to fix return top btn

---
+++ basket/basket.js
@@ -0,0 +1,18 @@
+function ReturnToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+var topButton = document.getElementById("TopBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    }
+    else {
+        topButton.style.display = "none";
+    }
+}
