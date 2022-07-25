4d67806c82e2ba3013afd904bb2054d78bd401e3
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-26 01:06:56 +0300

    added animations reveal and fade to basket

--- basket/basket.js
+++ basket/basket.js
@@ -17,7 +17,6 @@ function scrollFunction() {
     }
 }
 
-
 const hamBurger = document.querySelector(".hamburger");
 const nav_menu = document.querySelector(".nav-menu");
 
@@ -30,3 +29,24 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamBurger.classList.remove("active");
     nav_menu.classList.remove("active");
 }))
+
+
+function revealY() {
+    var reveals = document.querySelectorAll(".reveal-by-y");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("on");
+        }
+        else {
+            reveals[i].classList.remove("on");
+        }
+    }
+}
+
+window.addEventListener("scroll", revealY);
+window.addEventListener("pageshow", revealY);
