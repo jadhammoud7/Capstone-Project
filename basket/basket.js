0ec431677121984155e274ac17bdf4110af45ca2
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-25 23:56:48 +0300

    fixed hamburger menu in basket

--- basket/basket.js
+++ basket/basket.js
@@ -16,3 +16,17 @@ function scrollFunction() {
         topButton.style.display = "none";
     }
 }
+
+
+const hamBurger = document.querySelector(".hamburger");
+const nav_menu = document.querySelector(".nav-menu");
+
+hamBurger.addEventListener("click", () => {
+    hamBurger.classList.toggle("active");
+    nav_menu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+
+    hamBurger.classList.remove("active");
+    nav_menu.classList.remove("active");
+}))
