b54a36af65d58312a589aeae8ee93898aaa3e616
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-27 00:55:21 +0300

    fixed hamburger menu in appointments

--- maintenance_work/maintenance_work.js
+++ maintenance_work/maintenance_work.js
@@ -15,3 +15,17 @@ function ReturnToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+const hamBurger = document.querySelector(".hamburger");
+const nav_menu = document.querySelector(".nav-menu");
+
+hamBurger.addEventListener("click", () => {
+    hamBurger.classList.toggle("active");
+    nav_menu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+
+    hamBurger.classList.remove("active");
+    nav_menu.classList.remove("active");
+}))
