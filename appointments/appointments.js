db86e21cd264d1850ea30fce30bbc29de0b40a68
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-29 23:24:58 +0300

    fixed hamburger menu in appointments

--- appointments/appointments.js
+++ appointments/appointments.js
@@ -27,47 +27,73 @@ const about_menu = document.querySelector(".about_menu");
 const basket_menu = document.querySelector(".basket_menu");
 const myaccount_menu = document.querySelector(".myaccount_menu");
 
-var home_menu_innerHTML = home_menu.innerHTML;
-var shop_menu_innerHTML = shop_menu.innerHTML;
-var appointments_menu_innerHTML = appointments_menu.innerHTML;
-var contact_menu_innerHTML = contact_menu.innerHTML;
-var about_menu_innerHTML = about_menu.innerHTML;
-var basket_menu_innerHTML = basket_menu.innerHTML;
-var myaccount_menu_innerHTML = myaccount_menu.innerHTML;
-
+var home_menu_innerHTML = "";
+var shop_menu_innerHTML = "";
+var appointments_menu_innerHTML = "";
+var contact_menu_innerHTML = "";
+var about_menu_innerHTML = "";
+var basket_menu_innerHTML = "";
+var myaccount_menu_innerHTML = "";
+var clicks = 0;
 hamBurger.addEventListener("click", () => {
+    home_menu_innerHTML = home_menu.innerHTML;
+    shop_menu_innerHTML = shop_menu.innerHTML;
+    appointments_menu_innerHTML = appointments_menu.innerHTML;
+    contact_menu_innerHTML = contact_menu.innerHTML;
+    about_menu_innerHTML = about_menu.innerHTML;
+    basket_menu_innerHTML = basket_menu.innerHTML;
+    myaccount_menu_innerHTML = myaccount_menu.innerHTML;
+
     hamBurger.classList.toggle("active");
     nav_menu.classList.toggle("active");
+
+
     home_menu.classList.remove("home_menu");
-    home_menu.innerHTML += "Home";
     shop_menu.classList.remove("shop_menu");
-    shop_menu.innerHTML += "Shop";
     appointments_menu.classList.remove("appointments_menu");
-    appointments_menu.innerHTML += "Appointments";
     contact_menu.classList.remove("contact_menu");
-    contact_menu.innerHTML += "Contact Us";
     about_menu.classList.remove("about_menu");
-    about_menu.innerHTML += "About Us";
     basket_menu.classList.remove("basket_menu");
-    basket_menu.innerHTML += "My Basket";
     myaccount_menu.classList.remove("myaccount_menu");
-    myaccount_menu.innerHTML += "My Account";
+    if (clicks == 0) {
+        home_menu.innerHTML += "Home";
+        shop_menu.innerHTML += "Shop";
+        appointments_menu.innerHTML += "Appointments";
+        contact_menu.innerHTML += "Contact Us";
+        about_menu.innerHTML += "About Us";
+        basket_menu.innerHTML += "My Basket";
+        myaccount_menu.innerHTML += "My Account";
+    }
+
+    clicks++;
 })
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamBurger.classList.remove("active");
     nav_menu.classList.remove("active");
-    home_menu.classList.remove("home_menu");
-
+    home_menu.classList.add("home_menu");
+    home_menu.innerHTML = home_menu_innerHTML;
+    shop_menu.classList.add("shop_menu");
+    shop_menu.innerHTML = shop_menu_innerHTML;
+    appointments_menu.classList.add("appointments_menu");
+    appointments_menu.innerHTML = appointments_menu_innerHTML;
+    contact_menu.classList.add("contact_menu");
+    contact_menu.innerHTML = contact_menu_innerHTML;
+    about_menu.classList.add("about_menu");
+    about_menu.innerHTML = about_menu_innerHTML;
+    basket_menu.classList.add("basket_menu");
+    basket_menu.innerHTML = basket_menu_innerHTML;
+    myaccount_menu.classList.add("myaccount_menu");
+    myaccount_menu.innerHTML = myaccount_menu_innerHTML;
 }))
 
-function OpenGift(){
+function OpenGift() {
     const gift_div = document.getElementById("id01");
     const gift = document.querySelector(".present");
     gift_div.style.display = "block";
     gift.style.display = "none";
 }
 
-function CloseGift(){
+function CloseGift() {
     const gift_div = document.getElementById("id01");
     const gift = document.querySelector(".present");
     gift_div.style.display = "none";
