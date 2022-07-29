c9ca934b594c23343d128179b78dc5bbf11afbb5
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-30 00:38:08 +0300

    added fade and reveal animations for appoint home

--- home-page/home-page.js
+++ home-page/home-page.js
@@ -108,12 +108,14 @@ function revealY() {
 const about_us = document.querySelector(".about-us");
 const contact_us = document.querySelector(".contact-us");
 const testimonials = document.querySelector(".test-back");
+const appointments_paragraph = document.querySelector(".appointments");
 
 function AddRevealX() {
     var windowHeight = window.innerHeight;
     var elementTopAbout = about_us.getBoundingClientRect().top;
     var elementTopContact = contact_us.getBoundingClientRect().top;
     var elementTopTestimonials = testimonials.getBoundingClientRect().top;
+    var elementTopAppointments = appointments_paragraph.getBoundingClientRect().top;
     var elementVisible = -200;
     if (elementTopAbout < windowHeight - elementVisible) {
         about_us.classList.add("reveal-by-x");
@@ -126,6 +128,10 @@ function AddRevealX() {
     if (elementTopTestimonials < windowHeight - elementVisible) {
         testimonials.classList.add("reveal-by-x");
         testimonials.classList.add("fade");
+    }
+    if(elementTopAppointments < windowHeight - elementVisible){
+        appointments_paragraph.classList.add("reveal-by-x");
+        appointments_paragraph.classList.add("fade");
     }
 }
 
