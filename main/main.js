c1d66a154686c0d77cafa6518d43d1b092f96f5b
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-07-31 13:15:34 +0300

    updated main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,22 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
 
 const hamBurger = document.querySelector(".hamburger");
 const nav_menu = document.querySelector(".nav-menu");
@@ -81,16 +100,6 @@ function revealY() {
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("on");
         }
-    }
-}
-
-function AddRevealX(element) {
-    var windowHeight = window.innerHeight;
-    var elementTop = element.getBoundingClientRect().top;
-    var elementVisible = -200;
-    if (elementTopAbout < windowHeight - elementVisible) {
-        element.classList.add("reveal-by-x");
-        element.classList.add("fade");
     }
 }
 
