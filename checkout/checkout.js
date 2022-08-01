ac7c8cef65ee02981594048b5d529cccf105bb51
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-08-01 13:34:34 +0300

    added a function and put all code in it, working for the arrows in the calender

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -51,7 +51,39 @@ window.addEventListener("pageshow", AddRevealX);
 const date=new Date();
 
 const renderCalender=()=>{
-
+    const monthDays=document.querySelector('.days');
+    const lastDay=new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
+    console.log(lastDay);
+    const prevLastDay=new Date(date.getFullYear(),date.getMonth(),0).getDate();
+    const firstDayIndex=date.getDay();
+    const lastDayIndex=new Date(date.getFullYear(),date.getMonth()+1,0).getDay();
+    const nextDays=7-lastDayIndex-1;
+    const month=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    console.log(month);
+    document.querySelector('.date h1').innerHTML=month[date.getMonth()];
+    
+    
+    document.querySelector('.date p').innerHTML=date.toDateString();
+    
+    
+    let days="";
+    
+    for(let x=firstDayIndex; x>0;x--){
+        days+=`<div class="prev-date"> ${prevLastDay -x+1}</div>`;
+    }
+    for(let i=1;i<=lastDay;i++){
+        if(i===new Date().getDate() && date.getMonth()===new Date().getMonth()){
+            days+=`<div class="today"> ${i} </div>`;        
+        }else{
+            days+=`<div> ${i} </div>`;
+        }
+    
+    }
+    for(let j=1;j<=nextDays;j++){
+        days+=`<div class="next-date"> ${j}</div>`;
+        monthDays.innerHTML=days;
+    }
+    
 }
 
 
