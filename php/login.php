e27bb987b2d9b43c5db9376dcbf2ea86c51e19b5
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-08-10 13:59:04 +0300

    removed echo in sign up.php and working on login.php

--- php/login.php
+++ php/login.php
@@ -20,7 +20,8 @@ $statement = $connection->prepare("SELECT customer_id FROM customers WHERE usern
 $statement->execute();
 $statement_result = $statement->get_result();
 $data = $statement_result->fetch_assoc();
-$logged_id = $data['customer_id'];
+// $logged_id = $data['customer_id'];
+//saving which user is logged in
 
 $stmt = $connection->prepare("SELECT password FROM customers WHERE username='".$username."' ");
 $stmt->execute();
@@ -29,7 +30,7 @@ $stmt_data = $stmt_result->fetch_assoc();
 if($stmt_data['password'] == $password){
     // $_SESSION['logged_id'] = $logged_id;
     // $_SESSION['logged_bool'] = true;
-    header("Location: home-page.html");
+    header("Location:../home-page/home-page.php");
 }
 else{
     echo '<script> alert("<h2>Login Failed. Either email or password is invalid.</h2>") </script>';
--- php/signup.php
+++ php/signup.php
@@ -3,34 +3,26 @@
 include("connection.php");
 
 if(isset($_POST["first_name"]) && $_POST["first_name"] != ""){
-    $first_name = $_POST["first_name"];
-    echo $first_name;
-}
+    $first_name = $_POST["first_name"];}
 else{
     die("ALERT first_name");
 }
 
 if(isset($_POST["last_name"]) && $_POST["last_name"] != ""){
     $last_name = $_POST["last_name"];
-    echo $last_name;
-
 }
 else{
     die("ALERT last_name");
 }
 
 if(isset($_POST["email"]) && $_POST["email"] != ""){
-    $email = $_POST["email"];
-    echo $email;
-}
+    $email = $_POST["email"];}
 else{
     die("ALERT email");
 }
 
 if(isset($_POST["date_of_birth"]) && $_POST["date_of_birth"] != ""){
     $date_of_birth = $_POST["date_of_birth"];
-    echo $date_of_birth;
-
 }
 else{
     die("ALERT date_of_birth");
@@ -38,8 +30,6 @@ else{
 
 if(isset($_POST["phone_number"]) && $_POST["phone_number"] != ""){
     $phone_number = $_POST["phone_number"];
-    echo $phone_number;
-
 }
 else{
     die("ALERT phone_number");
@@ -47,8 +37,6 @@ else{
 
 if(isset($_POST["address"]) && $_POST["address"] != ""){
     $address = $_POST["address"];
-    echo $address;
-
 }
 else{
     die("ALERT address");
@@ -56,8 +44,6 @@ else{
 
 if(isset($_POST["username"]) && $_POST["username"] != ""){
     $username = $_POST["username"];
-    echo $username;
-
 }
 else{
     die("ALERT username");
@@ -65,8 +51,6 @@ else{
 
 if(isset($_POST["password"]) && $_POST["password"] != ""){
     $password = hash("sha256", $_POST["password"]);
-    echo $password;
-
 }
 else{
     die("ALERT password");
@@ -76,5 +60,5 @@ $mysql = $connection->prepare("INSERT INTO customers(first_name, last_name, emai
 $mysql->bind_param("ssssssss", $first_name, $last_name, $email, $date_of_birth, $phone_number, $address, $username, $password);
 $mysql->execute();
 $mysql->close();
-// header("Location:../home-page/home-page.php");
+header("Location:../home-page/home-page.php");
 ?>
