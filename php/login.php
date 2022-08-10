7533229be1875e83b07061b79554b3b1cbb2806d
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-08-10 11:50:52 +0300

    editing in login.php and in profile.html editing name and id values in the form

--- php/login.php
+++ php/login.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start();
+// session_start();
 include("connection.php");
 
 if(isset($_POST["user_name"]) && $_POST["user_name"] != ""){
@@ -27,8 +27,8 @@ $stmt->execute();
 $stmt_result = $stmt->get_result();
 $stmt_data = $stmt_result->fetch_assoc();
 if($stmt_data['password'] == $password){
-    $_SESSION['logged_id'] = $logged_id;
-    $_SESSION['logged_bool'] = true;
+    // $_SESSION['logged_id'] = $logged_id;
+    // $_SESSION['logged_bool'] = true;
     header("Location: home-page.html");
 }
 else{
