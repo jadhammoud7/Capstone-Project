b18aa04a429be4e843a6dfa1b208a02f5fc446fe
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-08-13 12:21:06 +0300

    edited php in input of first name in edit profile

--- php/editprofile.php
+++ php/editprofile.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
 include("../php/connection.php");
-$customer_id = $_SESSION['logged_id'];
-if (isset($_POST["first_name"])  && $_POST["first_name"] != "") {
-    $first_name = $_POST["first_name"];
-    $query = $mysqli->prepare("UPDATE customers SET first_name=? WHERE customer_id=$customer_id");
-    $query->bind_param("s", $first_name);
-    $query->execute();
+ $customer_id = $_SESSION['logged_id'];
+ if(isset($_POST["firstname_editprofile"])  && $_POST["firstname_editprofile"] != ""){
+    $first_name = $_POST["firstname_editprofile"];
 }
+$query1 = $connection->prepare("UPDATE customers SET first_name=? WHERE customer_id='".$customer_id."'");
+$query1->bind_param("s",$first_name);
+$query1->execute();
+?>
