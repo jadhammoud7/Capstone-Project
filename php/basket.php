8671f51cef3aaa9ba19354c90cd4f43eea8be834
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-08-18 10:00:18 +0300

    added price updating for basket php

--- php/basket.php
+++ php/basket.php
@@ -27,6 +27,14 @@ if(isset($_GET['productBasketID']) && isset($_GET['customerBasketID']) && isset(
     $stmt_update_quantity = $connection->prepare("UPDATE baskets_customer_product SET quantity = ? WHERE product_id = '" . $product_basket_id . "' and customer_id = '" . $customer_basket_id ."'");
     $stmt_update_quantity->bind_param("i", $_GET['quantity']);
     $stmt_update_quantity->execute();
+    $stmt_select_product_price = $connection->prepare("SELECT price FROM products WHERE product_id = '" . $product_basket_id . "'");
+    $stmt_select_product_price->execute();
+    $result_select_product_price = $stmt_select_product_price->get_result();
+    $row_select_product_price = $result_select_product_price->fetch_assoc();
+    $price = $row_select_product_price['price'];
+    $stmt_update_price = $connection->prepare("UPDATE baskets_customer_product SET price = ? WHERE product_id = '" . $product_basket_id . "' AND customer_id = '" . $customer_basket_id . "' ");
+    $stmt_update_price->bind_param("i", $price * $_GET['quantity']);
+    $stmt_update_price->execute();
     header("Location: '../basket/basket.php'");
 }
 
@@ -37,21 +45,35 @@ $row_select = $select_results->fetch_assoc();
 
 if(empty($row_select['product_id'])){
     $quantity = 1;
-    $stmt = $connection->prepare("INSERT INTO baskets_customer_product(customer_id, product_id, quantity) VALUES (?,?,?)");
-    $stmt->bind_param("iii", $customer_id, $product_id, $quantity);
+    $stmt_select_product_price = $connection->prepare("SELECT price FROM products WHERE product_id = '" . $product_id . "' ");
+    $stmt_select_product_price->execute();
+    $result_select_product_price = $stmt_select_product_price->get_result();
+    $row_select_product_price = $result_select_product_price->fetch_assoc();
+    $stmt = $connection->prepare("INSERT INTO baskets_customer_product(customer_id, product_id, quantity, price) VALUES (?,?,?,?)");
+    $stmt->bind_param("iii", $customer_id, $product_id, $quantity, $row_select_product_price['price']);
     $stmt->execute();
     $stmt->close();
     echo "<script>alert('Product added to basket'); window.location = '../shop/shop.php';</script>";
 }
 else{
-    $stmt_select_quantity = $connection->prepare("SELECT quantity FROM baskets_customer_product WHERE product_id = '". $product_id . "' ");
+    $stmt_select_quantity = $connection->prepare("SELECT quantity FROM baskets_customer_product WHERE product_id = '". $product_id . "' AND customer_id = '" . $customer_id ."' ");
     $stmt_select_quantity->execute();
     $select_quantity_results = $stmt_select_quantity->get_result();
     $row_select_quantity = $select_quantity_results->fetch_assoc();
     $quantity = $row_select_quantity['quantity'] + 1;
-    $stmt_update_quantity = $connection->prepare("UPDATE baskets_customer_product SET quantity = ? WHERE product_id = '" . $product_id . "'");
+    $stmt_update_quantity = $connection->prepare("UPDATE baskets_customer_product SET quantity = ? WHERE product_id = '" . $product_id . "' AND customer_id = '" . $customer_id ."'");
     $stmt_update_quantity->bind_param("i", $quantity);
     $stmt_update_quantity->execute();
+
+    $stmt_select_price = $connection->prepare("SELECT price FROM baskets_customer_product WHERE product_id = '". $product_id . "' AND customer_id = '" . $customer_id ."'");
+    $stmt_select_price->execute();
+    $select_price_results = $stmt_select_price->get_result();
+    $row_select_price = $select_price_results->fetch_assoc();
+    $price = $row_select_price['price'] * 2;
+    $stmt_update_price = $connection->prepare("UPDATE baskets_customer_product SET price = ? WHERE product_id = '" . $product_id . "' AND customer_id = '" . $customer_id ."'");
+    $stmt_update_price->bind_param("i", $price);
+    $stmt_update_price->execute();
+
     echo "<script>alert('Product already in basket. Quantity added by 1.'); window.location = '../shop/shop.php';</script>";
 
 }
