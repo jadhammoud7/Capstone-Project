b2ed81b3007df63d2ecf0f7b9d80987fa446b5f9
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-08-22 22:23:04 +0300

    adding new db table (repair) so connect repair page to the db

---
+++ php/repair.php
@@ -0,0 +1,14 @@
+<?php
+
+include("connection.php");
+
+//for basket
+function repair_products($repair_t, $price_per_hour, $description)
+{
+
+    $element = "
+    
+    "
+    echo $element;
+}
+?>
