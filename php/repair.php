61c4ace5c422d4de947dc3e6a52e18c61fd18000
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-08-25 19:52:23 +0300

    edited name and id for every radio button

--- php/repair.php
+++ php/repair.php
@@ -57,17 +57,17 @@ function book_now_for_each_repair_connection($repair_type,$price_per_hour,$descr
     <div class=\"appointment-item-info-schedule\">
         <img src=\"../images/ps_repair.jpg\" alt=\"\">
         <div class=\"appointment-item-info-schedule-part\">
-        <input type=\"radio\" id=\"html\" name=\"fav_language\" value=\"8:00-9:30 AM\">
+        <input type=\"radio\" id=\"app1\" name=\"app1\" value=\"8:00-9:30 AM\">
         <label for=\"html\">8:00-9:30 AM</label><br>
-        <input type=\"radio\" id=\"css\" name=\"fav_language\" value=\"10:00-11:30 AM\">
+        <input type=\"radio\" id=\"app2\" name=\"app2\" value=\"10:00-11:30 AM\">
         <label for=\"css\">10:00-11:30 AM</label><br>
-        <input type=\"radio\" id=\"css\" name=\"fav_language\" value=\"12:00-1:30 PM\">
+        <input type=\"radio\" id=\"app3\" name=\"app3\" value=\"12:00-1:30 PM\">
         <label for=\"css\">10:00-11:30 AM</label><br>
-        <input type=\"radio\" id=\"css\" name=\"fav_language\" value=\"2:00-3:30 PM\">
+        <input type=\"radio\" id=\"app4\" name=\"app4\" value=\"2:00-3:30 PM\">
         <label for=\"css\">10:00-11:30 AM</label><br>
-        <input type=\"radio\" id=\"css\" name=\"fav_language\" value=\"4:00-5:30 PM\">
+        <input type=\"radio\" id=\"app5\" name=\"app5\" value=\"4:00-5:30 PM\">
         <label for=\"css\">10:00-11:30 AM</label><br>
-        <input type=\"radio\" id=\"css\" name=\"fav_language\" value=\"6:00-7:30 PM\">
+        <input type=\"radio\" id=\"app6\" name=\"app6\" value=\"6:00-7:30 PM\">
         <label for=\"css\">10:00-11:30 AM</label><br>
         </div>
         <button style=\"border-radius:15px\">Schedule your Appointment</button>
