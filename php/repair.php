d46ca2869901226c3980edb235d4ec3f3e89e692
Mohamad Nabaa <mohamad.nabaa01@lau.edu>
2022-08-25 13:59:13 +0300

    Update repair.php

--- php/repair.php
+++ php/repair.php
@@ -9,7 +9,6 @@ function repair_products_connection($repair_type, $price_per_hour, $description)
     $element = "
     <div class=\"appointment-item\">
     <h2>$repair_type</h2>
-    <form action=\"../appointments/appointments.php\" method=\"POST\">
     <div class=\"appointment-item-info\">
         <img src=\"../images/laptop_repair.png\" alt=\"\">
         <div class=\"appointment-item-info-part\">
@@ -20,7 +19,7 @@ function repair_products_connection($repair_type, $price_per_hour, $description)
     </div>
     <div>
     <a href=\"../calendar/calendar.php?repair_type=$repair_type\">
-    <button type=\"submit\" onclick=\"OpenAppointmentBooking(this)\" style=\"border-radius: 15px;color:black ;\"><strong>Book Now</strong></button>
+        <button onclick=\"OpenAppointmentBooking(this)\" style=\"border-radius: 15px;color:black ;\"><strong>Book Now</strong></button>
     </a>
     </div>
 </div>
