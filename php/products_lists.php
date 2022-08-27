75ce8a0045bc2274861b7abf0f41e0de9bcc9d0e
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-08-27 12:48:07 +0300

    fixed appointments list style in profile page

--- php/products_lists.php
+++ php/products_lists.php
@@ -36,18 +36,40 @@ function favorites_list_connection($product_id, $name, $category, $price)
 function appointments_list_connection($appointment_id, $appointment_name, $date, $hour)
 {
     //dont forget to add thr image
+    // $element =  "
+    //     <div class=\"appointmentsss\">
+    //         <img src=\"../images/console.png\" alt=\"\" style=\"width:50%\">
+    //         <div class=\"app_info\">
+    //             <h4><b>$appointment_name </b></h4>
+    //             <p>Date: $date</p>
+    //             <p>Hours: $hour</p>
+    //             <button onclick=\"window.location=\"../profile/profile.php?deleteAPPid=$appointment_id;\"\" class=\"remove_app\"><strong>Remove Appointment</strong></button>
+    //         </div>
+    //     </div>
+    // ";
 
-    $element =  "
-    <div class=\"appointmentsss\">
-    <img src=\"../images/console.png\" alt=\"\" style=\"width:50%\">
-    <div class=\"app_info\">
-        <h4><b>$appointment_name </b></h4>
-        <p>Date: $date</p>
-        <p>Hours: $hour</p>
-        <button onclick=\"window.location=\"../profile/profile.php?deleteAPPid=$appointment_id;\"\" class=\"remove_app\"><strong>Remove Appointment</strong></button>
-    </div>
-</div>
-";
-
+    $element = "
+        <div class=\"appointments-list\">
+            <div class=\"appointments-div\">
+                <div class=\"appointments-img\">
+                    <img src=\"../images/console.png\" alt=\"basket product\" style=\"width: 50%;\">
+                </div>
+                <div class=\"appointments-part\">
+                    <h3>Appointment Name</h3>
+                    <h4>$appointment_name</h4>
+                </div>
+                <div class=\"appointments-part\">
+                    <h3>Date</h3>
+                    <h4>$date</h4>
+                </div>
+                <div class=\"appointments-part\">
+                    <h3>Hour</h3>
+                    <h4>$hour</h4>
+                </div>
+            </div>
+            <div class=\"appointments-button\">
+                <button onclick=\"window.location=\"../profile/profile.php?deleteAPPid=$appointment_id;\"\" class=\"remove_app\"><i class=\"fa fa-remove\"></i><strong>Delete Appointment</strong></button>
+            </div>
+        </div>";
     echo $element;
 }
