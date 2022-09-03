cdbbbc0cc102a942e1811cf3d01929ac34c83028
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-09-03 14:55:14 +0300

    updated about us css

--- aboutus/aboutus.php
+++ aboutus/aboutus.php
@@ -16,7 +16,6 @@ if (!isset($_SESSION['logged_bool'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../main/main.css">
-    <link rel="stylesheet" href="../contactus/contactus.css">
     <link rel="stylesheet" href="../aboutus/aboutus.css">
     <title>About Us - Newbies Gamers</title>
 </head>
