781604a94e8803b62f48aa27490dbbc3e23bfbe6
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-09-08 22:25:12 +0300

    fixed styling errors in home admin

--- home-admin/home-admin.php
+++ home-admin/home-admin.php
@@ -9,7 +9,6 @@ if (!isset($_SESSION['logged_bool'])) {
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,31 +20,24 @@ if (!isset($_SESSION['logged_bool'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link rel="stylesheet" href="../home-admin/home-admin.css">
-    <link rel="stylesheet" href="../home-page/home-page.css">
-    <link rel="stylesheet" href="../main/main.css">
+    <!-- <link rel="stylesheet" href="../home-page/home-page.css"> -->
+    <!-- <link rel="stylesheet" href="../main/main.css"> -->
     <title>Home Admin - Newbies Gamers</title>
 </head>
 
 <body onunload="myFunction()">
-    <!-- started with the menu bar -->
-    <!-- <header>
-        <nav class="nav-bar" id="hhh">
-            <a href="" class="nav-branding">Newbie Gamers.</a>
-        </nav>
-    </header> -->
-    <!-- ended with the menu bar -->
 
     <!-- started popup message login successful -->
-    <div class="popup" id="login-confirmation">
+    <!-- <div class="popup" id="login-confirmation">
         <img src="../images/tick.png" alt="successfully logged in">
         <h2>Login Successful</h2>
         <p>Welcome to Newbies Gamers</p>
         <button type="button" onclick="RemoveLogInPopUp()">OK</button>
-    </div>
+    </div> -->
 
     <input type="checkbox" id="nav-toggle">
     <div class="sidebar">
-        <div class="sidebar-header">
+        <div class="sidebar-brand">
             <h2>
                 <span class="lab la-newbiesgamers"></span> <span>Newbies Gamers</span>
             </h2>
@@ -95,16 +87,14 @@ if (!isset($_SESSION['logged_bool'])) {
     </div>
 
     <div class="main-content">
-        <div class="header">
+        <header>
             <h2>
                 <label for="nav-toggle">
-                    <span><i class="fa fa-bars"></i></span>
+                    <span><i class="las la-bars"></i></span>
                 </label>
 
                 Dashboard
             </h2>
-
-
 
             <div class="user-wrapper">
                 <img src="../images/info.png" width="40px" height="40px" alt="">
@@ -113,17 +103,17 @@ if (!isset($_SESSION['logged_bool'])) {
                     <small>Admin</small>
                 </div>
             </div>
-        </div>
+        </header>
 
         <main>
-            <div class="dashboard-cards">
+            <div class="cards">
                 <div class="card-single">
                     <div>
                         <h1>54</h1>
                         <span>Customers</span>
                     </div>
                     <div>
-                        <span><i class="fa fa-user"></i></span>
+                        <span class="las la-users"></span>
                     </div>
                 </div>
                 <div class="card-single">
@@ -132,16 +122,16 @@ if (!isset($_SESSION['logged_bool'])) {
                         <span>Appointments</span>
                     </div>
                     <div>
-                        <span><i class="fa fa-calendar"></i></span>
+                        <span class="las la-clipboard"></span>
                     </div>
                 </div>
                 <div class="card-single">
                     <div>
                         <h1>124</h1>
-                        <span><i class="fa fa-receipt"></i>Chekouts</span>
+                        <span>Chekouts</span>
                     </div>
                     <div>
-                        <span><i class="fa fa-shopping-bag" aria-hidden="true"></i></span>
+                        <span class="las la-shopping-bag"></span>
                     </div>
                 </div>
                 <div class="card-single">
@@ -150,7 +140,7 @@ if (!isset($_SESSION['logged_bool'])) {
                         <span>Income</span>
                     </div>
                     <div>
-                        <span><i class="fa fa-google-wallet"></i></span>
+                        <span class="las la-google-wallet"></span>
                     </div>
                 </div>
             </div>
@@ -159,7 +149,7 @@ if (!isset($_SESSION['logged_bool'])) {
                     <div class="card">
                         <div class="card-header">
                             <h3>Recent Projects</h3>
-                            <button>See all <span><i class="fa fa-arrow-right"></i></span></button>
+                            <button>See all <span class="las la-arrow-right"></span></button>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
@@ -169,7 +159,6 @@ if (!isset($_SESSION['logged_bool'])) {
                                             <td>Project Titles</td>
                                             <td>Department</td>
                                             <td>Status</td>
-
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -255,7 +244,7 @@ if (!isset($_SESSION['logged_bool'])) {
                     <div class="card">
                         <div class="card-header">
                             <h3>New Customer</h3>
-                            <button>See all <span><i class="fa fa-arrow-right"></i></span></button>
+                            <button>See all <span class="las la-arrow-right"></span></button>
                         </div>
                         <div class="card-body">
                             <div class="customer">
@@ -267,9 +256,9 @@ if (!isset($_SESSION['logged_bool'])) {
                                     </div>
                                 </div>
                                 <div class="contact">
-                                    <span><i class="fa fa-circle-user"></i></span>
-                                    <span><i class="fa fa-comment-alt"></i></span>
-                                    <span><i class="fa fa-phone"></i></span>
+                                    <span><i class="las la-circle-user"></i></span>
+                                    <span><i class="las la-comment"></i></span>
+                                    <span><i class="las la-phone"></i></span>
                                 </div>
                             </div>
                             <div class="customer">
@@ -281,9 +270,9 @@ if (!isset($_SESSION['logged_bool'])) {
                                     </div>
                                 </div>
                                 <div class="contact">
-                                    <span><i class="fa fa-circle-user"></i></span>
-                                    <span><i class="fa fa-comment-alt"></i></span>
-                                    <span><i class="fa fa-phone"></i></span>
+                                    <span><i class="las la-circle-user"></i></span>
+                                    <span><i class="las la-comment"></i></span>
+                                    <span><i class="las la-phone"></i></span>
                                 </div>
                             </div>
                             <div class="customer">
@@ -295,9 +284,9 @@ if (!isset($_SESSION['logged_bool'])) {
                                     </div>
                                 </div>
                                 <div class="contact">
-                                    <span><i class="fa fa-circle-user"></i></span>
-                                    <span><i class="fa fa-comment-alt"></i></span>
-                                    <span><i class="fa fa-phone"></i></span>
+                                    <span><i class="las la-circle-user"></i></span>
+                                    <span><i class="las la-comment"></i></span>
+                                    <span><i class="las la-phone"></i></span>
                                 </div>
                             </div>
                             <div class="customer">
@@ -309,9 +298,9 @@ if (!isset($_SESSION['logged_bool'])) {
                                     </div>
                                 </div>
                                 <div class="contact">
-                                    <span><i class="fa fa-circle-user"></i></span>
-                                    <span><i class="fa fa-comment-alt"></i></span>
-                                    <span><i class="fa fa-phone"></i></span>
+                                    <span><i class="las la-circle-user"></i></span>
+                                    <span><i class="las la-comment"></i></span>
+                                    <span><i class="las la-phone"></i></span>
                                 </div>
                             </div>
                         </div>
@@ -327,11 +316,6 @@ if (!isset($_SESSION['logged_bool'])) {
     <!-- started return to top button -->
     <button onclick="ReturnToTop()" id="TopBtn" title="Return to Top"><i class="fa fa-arrow-up"></i></button>
     <!-- ended return to top button -->
-
-
-<!-- 
-    <script src="../home-page/home-page.js"></script>
-    <script src="../main/main.js"></script> -->
 
 </body>
 
