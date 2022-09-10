b3a7c6f8973993a44fb5a08585729cd4ee04aa67
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-09-10 11:56:52 +0300

    finished editing the buttons and changing its design when onclick through js

--- home-admin/home_admin.js
+++ home-admin/home_admin.js
@@ -1,17 +1,15 @@
-const btn=document.getElementById('btn_done_work');
-
-
-btn.addEventListener('click', function handleClick() {
+const btn=document.getElementsByClassName('btn_done_work');
+for(let i=0;i<btn.length;i++){
     const initialText="Done Work";
-    if(btn.textContent.toLowerCase().includes(initialText.toLowerCase())){
-        btn.textContent="Return to pending";
-        btn.style.color="black";
-        btn.style.background="red";
-    }else{
-        btn.textContent=initialText;
-        btn.style.color="white";
-        btn.style.background="royalblue";
-    }
-
-
-  });
+    btn[i].addEventListener('click',function handleClick(){
+        if(btn[i].textContent.toLowerCase().includes(initialText.toLowerCase())){
+            btn[i].textContent="Return to pending";
+            btn[i].style.color="black";
+            btn[i].style.background="red";
+        }else{
+            btn[i].textContent=initialText;
+            btn[i].style.color="white";
+            btn[i].style.background="royalblue";
+        }
+    });    
+}
