2ecee2ad89101385e9b4bb719805e645c4f42a42
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-10-15 17:19:47 +0300

    exported db

--- newbie_gamers (21).sql
+++ newbie_gamers (21).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2022 at 02:01 AM
+-- Generation Time: Oct 15, 2022 at 04:19 PM
 -- Server version: 8.0.23
 -- PHP Version: 8.0.2
 
@@ -201,7 +201,7 @@ CREATE TABLE `customers` (
 INSERT INTO `customers` (`customer_id`, `first_name`, `last_name`, `email`, `date_of_birth`, `phone_number`, `address`, `city`, `username`, `password`, `loyalty_points`) VALUES
 (2, 'jad', 'hammoud', 'jad.hammoud@gmail.com', '2022-09-06', '76939605', 'beirut ras el nabeh', 'aaramun', 'jad', '8384caf9895a1f9ab17aa8055b0b5869f3e8eba263aac96585f1ee871dd3d5f0', 0),
 (3, 'mohamad', 'Nabaa', 'mohamad@gmail.com', '2022-08-22', '71123805', 'beirut next to fakhani second floor', 'bshamoun', 'mhmd', '8f9cebbfdc1a99ce7a4941ad08c34c4f1f08089ceff43b802dab3b951d6cbfd1', 0),
-(4, 'Mohamad', 'Nabaa', 'mohamad.nabaa01@lau.edu', '2022-08-01', '+96171123805', 'Aramoun next to chamsine third floor', 'aaramun', 'Mohamad Nabaa', '578835a5afad634f5716badf3d801e8910dec33e73ec5c9e86b8d409f229263d', 50),
+(4, 'Mohamad', 'Nabaa', 'mohamad.nabaa01@lau.edu', '2022-08-01', '+96171123805', 'Aramoun next to chamsine third floor', 'aaramun', 'Mohamad Nabaa', '578835a5afad634f5716badf3d801e8910dec33e73ec5c9e86b8d409f229263d', 91),
 (16, 'Mohamad', 'Nabaa', 'mohamad.nabaa01@lau.edu', '2001-07-18', '96171123805', 'Aramoun next to chamsine third floor', 'beirut', 'Mohamad Nabaa', '578835a5afad634f5716badf3d801e8910dec33e73ec5c9e86b8d409f229263d', 0),
 (17, 'Omar ', 'Atieh', 'omar4Atieh@hotmail.com', '2022-09-21', '878887878788', 'Bshamoun , al wadi street Al nader building block B second floor', 'bshamoun', 'omar1234567', '75523907535270f2f12668aea07b507433e81ed0e625b9455bdb8554855687e0', 0),
 (21, 'Ahmad', 'Serhan', 'ahmadserhan@gmail.com', '2022-08-30', '45657888878', 'Bshamoun , al wadi street Al nader building block B second floor', 'beirut', 'ahmad1231234', '68a8b4c8464fa9bc797ce97b47bff6807ba10d8ddb061fce39b28842389888fa', 0);
@@ -243,7 +243,16 @@ CREATE TABLE `history_product_inventory` (
 --
 
 INSERT INTO `history_product_inventory` (`product_id`, `inventory`, `inventory_change`, `modified_by`, `modified_on`) VALUES
-(1, 98, '', '', '');
+(1, 98, '', '', ''),
+(19, 5, '0', 'Mohamad Nabaa', '2022-10-15 01:39:22'),
+(19, 5, '0', 'Mohamad Nabaa', '2022-10-15 01:47:26'),
+(20, 5, '0', 'Mohamad Nabaa', '2022-10-15 01:53:23'),
+(20, 10, '+ 5', 'Mohamad Nabaa', '2022-10-15 01:57:55'),
+(20, 10, '+ 5', 'Mohamad Nabaa', '2022-10-15 02:03:18'),
+(20, 10, '+ 5', 'Mohamad Nabaa', '2022-10-15 02:08:08'),
+(1, 86, '- 10', 'Store Sales', '2022-10-15 05:02:30'),
+(1, 81, '- 5', 'Store Sales', '2022-10-15 05:03:27'),
+(1, 80, '- 1', 'Store Sales', '2022-10-15 05:05:34');
 
 -- --------------------------------------------------------
 
@@ -271,7 +280,13 @@ INSERT INTO `history_product_prices` (`product_id`, `price`, `price_change`, `mo
 (1, 250, '', 'Mohamad Nabaa', '2022-10-11 12:32:14'),
 (1, 300, '', 'Mohamad Nabaa', '2022-10-11 12:33:33'),
 (1, 200, '', 'Mohamad Nabaa', '2022-10-11 12:38:22'),
-(1, 300, '', 'Mohamad Nabaa', '2022-10-11 12:39:35');
+(1, 300, '', 'Mohamad Nabaa', '2022-10-11 12:39:35'),
+(19, 30, '0', 'Mohamad Nabaa', '2022-10-15 01:39:22'),
+(19, 30, '0', 'Mohamad Nabaa', '2022-10-15 01:47:26'),
+(20, 30, '0', 'Mohamad Nabaa', '2022-10-15 01:53:23'),
+(20, 25, '- 5', 'Mohamad Nabaa', '2022-10-15 01:57:55'),
+(20, 25, '- 5', 'Mohamad Nabaa', '2022-10-15 02:03:18'),
+(20, 25, '- 5', 'Mohamad Nabaa', '2022-10-15 02:08:08');
 
 -- --------------------------------------------------------
 
@@ -292,7 +307,13 @@ CREATE TABLE `history_product_sales` (
 --
 
 INSERT INTO `history_product_sales` (`product_id`, `sales_number`, `sales_change`, `modified_by`, `modified_on`) VALUES
-(1, 5, '', '', '2022-10-11');
+(1, 5, '', '', '2022-10-11'),
+(19, 0, '0', 'Mohamad Nabaa', '2022-10-15 01:39:22'),
+(19, -3, '0', 'Mohamad Nabaa', '2022-10-15 01:47:26'),
+(20, 0, '0', 'Mohamad Nabaa', '2022-10-15 01:53:23'),
+(1, 1, '+ 10', 'Store Sales', '2022-10-15 05:02:30'),
+(1, 5, '+ 5', 'Store Sales', '2022-10-15 05:03:27'),
+(1, 1, '+ 1', 'Store Sales', '2022-10-15 05:05:34');
 
 -- --------------------------------------------------------
 
@@ -320,7 +341,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `name`, `price`, `type`, `category`, `description`, `age`, `image`, `inventory`, `sales_number`, `last_modified_by`, `last_modified_on`) VALUES
-(1, 'Doom Eternal', 300, 'cds', 'XBOX Cd', 'Hell’s armies have invaded Earth. Become the Slayer in an epic single-player campaign to conquer demons across dimensions and stop the final destruction of humanity. The only thing they fear... is you.', '15+', '', 96, 4, 'Mohamad Nabaa', '2022-10-11 12:39:35'),
+(1, 'Doom Eternal', 300, 'cds', 'XBOX Cd', 'Hell’s armies have invaded Earth. Become the Slayer in an epic single-player campaign to conquer demons across dimensions and stop the final destruction of humanity. The only thing they fear... is you.', '15+', '', 80, 5, 'Store Sales', '2022-10-15 05:05:34'),
 (2, 'Gears 5', 46, 'cds', 'XBOX Cd', 'Gears 5 follows the story of Kait Diaz, who is on a journey to find out the origin of the Locust Horde, the main antagonistic faction of the Gears of War series.', '16+', '', 0, 0, '', ''),
 (3, 'Watch Dogs: Legion', 50, 'cds', 'XBOX Cd', 'Gameplay in the Watch Dogs games focuses on an open world where the player can complete missions to progress an overall story, as well as engage in various side activities.', '12+', '', 0, 0, '', ''),
 (4, 'Battletoads', 50, 'cds', 'XBOX Cd', 'After being locked up in a fantasy simulator bunker for 26 years, the Battletoads are no longer intergalactic heroes and have fallen into modern day obscurity. Unable to settle down for a quiet, simple life, they set out to once again defeat their old longtime nemesis, The Dark Queen, to regain their lost fame. But when they confront the Queen they find out she had been in a similar predicament as them, having also been trapped and losing her powers. In the end they decide to team up with her to take down an evil alien race called the Topians, who were responsible for trapping all of them and are now the current rulers of the galaxy.', '12+', '', 0, 0, '', ''),
@@ -330,15 +351,7 @@ INSERT INTO `products` (`product_id`, `name`, `price`, `type`, `category`, `desc
 (8, 'Sonic', 46, 'cds', 'PS3 Cd', 'Sonic the Hedgehog CD is a 1993 platform game for the Sega CD developed and published by Sega. The story follows Sonic the Hedgehog as he attempts to save an extraterrestrial body, Little Planet, from Doctor Robotnik. Like other Sonic games, Sonic runs and jumps through several themed levels while collecting rings and defeating robots. Sonic CD is distinguished by its time travel feature, a key aspect to the story and gameplay. By traveling through time, players can access different versions of stages, featuring alternative layouts, music, and graphics.', '9+', '', 0, 0, '', ''),
 (9, 'PlayStation 3', 300, 'console', 'PS3', 'The PlayStation 3 (PS3) is a home video game console developed by Sony Computer Entertainment. The successor to the PlayStation 2, it is part of the PlayStation brand of consoles. It was first released on November 11, 2006, in Japan, November 17, 2006, in North America, and March 23, 2007, in Europe and Australia. The PlayStation 3 competed primarily against Microsoft\'s Xbox 360 and Nintendo\'s Wii as part of the seventh generation of video game consoles.', 'Any', '', 0, 0, '', ''),
 (10, 'PlayStation 4', 550, 'console', 'PS4', 'The PlayStation 4 (PS4) is a home video game console developed by Sony Computer Entertainment. Announced as the successor to the PlayStation 3 in February 2013, it was launched on November 15, 2013, in North America, November 29, 2013 in Europe, South America and Australia, and on February 22, 2014 in Japan. A console of the eighth generation, it competes with Microsoft\'s Xbox One and Nintendo\'s Wii U and Switch.', 'Any', '', 0, 0, '', ''),
-(11, 'qqq', 1, 'qqq', 'qqq', 'qqqqq', '12+ age', 'image.png', 12, 0, '', ''),
-(12, 'qqq', 12, 'qqq', 'qqq', 'qqqqq', '12+ age', 'image.png', 12, 0, '', ''),
-(13, 'qqq', 12, 'qqq', 'qqq', 'qqqqq', '12+ age', '', 12, 0, '', ''),
-(14, 'qqq', 12, 'qqq', 'qqq', 'qqqqq', '12+ age', '', 12, 0, '', ''),
-(15, 'qqq', 12, 'qqq', 'qqq', 'qqqqq', '12+ age', '', 12, 0, '', ''),
-(16, 'qqq', 12, 'qqq', 'qqq', 'qqqqq', '12+ age', '', 12, 0, '', ''),
-(17, 'qqq', 12, 'qqq', 'qqq', 'qqqqq', '12+ age', 'circuit.jpg', 12, 0, '', ''),
-(18, 'qqq', 12, 'qqq', 'qqq', 'qqqqq', '12+ age', 'circuit.jpg', 12, 0, '', ''),
-(19, 'qqq', 12, 'qqq', 'qqq', 'qqqqq', '12+ age', 'circuit.jpg', 12, 0, '', '');
+(20, 'GTA 5', 25, 'cds', 'strategy', 'Grand Theft Auto V is a 2013 action-adventure game developed by Rockstar North and published by Rockstar Games. It is the seventh main entry in the Grand Theft Auto series, following 2008\'s Grand Theft Auto IV, and the fifteenth instalment overall', '16+', 'GTA5.jpg', 10, 0, 'Mohamad Nabaa', '2022-10-15 02:08:08');
 
 -- --------------------------------------------------------
 
@@ -398,7 +411,14 @@ INSERT INTO `sales_products` (`sales_id`, `product_name`, `quantity`, `price`) V
 (19, 'Doom Eternal', 5, 1500),
 (20, 'Watch Dogs: Legion', 10, 500),
 (21, 'Watch Dogs: Legion', 10, 500),
-(22, 'Gears 5', 10, 460);
+(22, 'Gears 5', 10, 460),
+(23, 'Doom Eternal', 5, 1500),
+(23, 'Doom Eternal', 5, 1500),
+(24, 'Doom Eternal', 5, 1500),
+(25, 'Doom Eternal', 10, 3000),
+(26, 'Doom Eternal', 10, 3000),
+(27, 'Doom Eternal', 5, 1500),
+(28, 'Doom Eternal', 1, 300);
 
 -- --------------------------------------------------------
 
@@ -445,7 +465,13 @@ INSERT INTO `store_sales` (`store_sales_id`, `customer_name`, `username`, `email
 (19, 'Mohamad Nabaa', 'Mohamad Nabaa', 'mnabaa53@gmail.com', 1, 5, 1500, '2022-10-11 01:33:44'),
 (20, 'Mohamad Nabaa', 'Mohamad Nabaa', 'mnabaa53@gmail.com', 1, 10, 500, '2022-10-11 01:34:18'),
 (21, 'Mohamad Nabaa', 'Mohamad Nabaa', 'mnabaa53@gmail.com', 1, 10, 500, '2022-10-11 01:34:37'),
-(22, 'Mohamad Nabaa', 'Mohamad Nabaa', 'mnabaa53@gmail.com', 1, 10, 460, '2022-10-11 01:39:08');
+(22, 'Mohamad Nabaa', 'Mohamad Nabaa', 'mnabaa53@gmail.com', 1, 10, 460, '2022-10-11 01:39:08'),
+(23, 'Mohamad Nabaa', 'Mohamad Nabaa', 'mnabaa53@gmail.com', 1, 5, 1500, '2022-10-15 04:28:48'),
+(24, 'Mohamad Nabaa', 'Mohamad Nabaa', 'mnabaa53@gmail.com', 1, 5, 1500, '2022-10-15 04:32:55'),
+(25, 'Mohamad Nabaa', 'Mohamad Nabaa', 'mnabaa53@gmail.com', 1, 10, 3000, '2022-10-15 04:58:50'),
+(26, 'Mohamad Nabaa', 'Mohamad Nabaa', 'mnabaa53@gmail.com', 1, 10, 3000, '2022-10-15 05:02:30'),
+(27, 'Mohamad Nabaa', 'Mohamad Nabaa', 'mnabaa53@gmail.com', 1, 5, 1500, '2022-10-15 05:03:28'),
+(28, 'Mohamad Nabaa', 'Mohamad Nabaa', 'mnabaa53@gmail.com', 1, 1, 300, '2022-10-15 05:05:34');
 
 --
 -- Indexes for dumped tables
@@ -490,6 +516,12 @@ ALTER TABLE `repairs`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `repairs`
